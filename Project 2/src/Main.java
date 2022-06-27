83a00d108983a853e7ee383bc732834a5cf3c4e9
Denis Perdomo <joanp307@gmail.com>
2022-06-27 17:05:56 -0400

    Project 3 changes to Project 2

--- Project 2/src/Main.java
+++ Project 2/src/Main.java
@@ -1,9 +1,65 @@
 import java.util.Random;
 import java.util.Scanner;
 
+// Same functionality as a Scanner,
+// but with added checks
+class MyInput {
+    Scanner input;
+
+    MyInput() {
+        input = new Scanner(System.in);
+    }
+
+    public int nextInt() {
+        int out;
+
+        try {
+            out = input.nextInt();
+        } catch (Exception err) {
+            System.out.println("Your input was not a valid integer. Try again:");
+            while (true) {
+                input.nextLine();
+                try {
+                    out = input.nextInt();
+                    break;
+                } catch (Exception secondErr) {
+                    System.out.println("Try once again:");
+                }
+            }
+        }
+
+        return out;
+    }
+
+    public String nextLine() {
+        String out;
+        try {
+            out = input.nextLine();
+        } catch (Exception err) {
+            System.out.println("Your input was not valid. Try again:");
+            while (true) {
+                input.nextLine();
+                try {
+                    out = input.nextLine();
+                    break;
+                } catch (Exception secondErr) {
+                    System.out.println("Try once again:");
+                }
+            }
+        }
+
+        return out;
+    }
+
+    public String nextLnUnsafe() {
+        return input.nextLine();
+    }
+}
+
 class Main {
-    private static void game(Scanner input, String name) {
-        int answer = new Random().nextInt(1, 21);
+
+    private static void game(MyInput input, String name) {
+        int answer = new Random().nextInt(1, 20+1);
 
         System.out.println("\nWell, " + name + ", I am thinking of a number between 1 and 20.\nTake a guess.\n");
         int guess = input.nextInt();
@@ -33,13 +89,14 @@ class Main {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        MyInput input = new MyInput();
         System.out.println("Hello! What is your name?\n");
         String name = input.nextLine();
 
         while (true) {
             game(input, name);
             System.out.println("Would you like to play again? (y or n)\n");
+            input.nextLine(); // consuming the \n
 
             String repeat = input.nextLine().toLowerCase();
             System.out.println();
